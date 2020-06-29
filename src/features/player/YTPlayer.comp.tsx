f1e47a0ac88372bbe821c00ec75530671069f3ff
Ivan Pkh <ivanoid768@users.noreply.github.com>
2020-06-29 21:43:13 +0500

    YTPlayer via react-youtube package

--- src/features/player/YTPlayer.comp.tsx
+++ src/features/player/YTPlayer.comp.tsx
@@ -1,8 +1,23 @@
 import React from "react"
+import YouTube from 'react-youtube';
 
-export const YTPlayer = () => {
+const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 0 as 0 | 1 | undefined,
+    },
+};
+
+const onReady = (event: { target: any }) => {
+    console.log(event.target)
+}
+
+export const YTPlayer = ({ url }: { url?: string }) => {
+    let videoId = url?.split('v=')[1]
 
     return (
-        <div id="embed_yt_player_id">YTPlayer</div>
+        <YouTube id='embed_yt_player_id' videoId={videoId} opts={opts} onReady={onReady} />
     )
 }
